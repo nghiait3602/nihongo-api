9afb4ea8428182d88da7b663ca28b712afab6982
Nguyễn Trung Nghĩa (Nan) <104846298+nghiait3602@users.noreply.github.com>
2024-03-04 21:27:53 +0700

    Update handlerFactory.js

--- controller/handlerFactory.js
+++ controller/handlerFactory.js
@@ -1,6 +1,6 @@
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('./../utils/appError');
-/// đây chỉ là demo
+/// đây chỉ là demo test
 exports.deleteOne = (Model) =>
   catchAsync(async (req, res, next) => {
     const doc = await Model.findByIdAndDelete(req.params.id);
