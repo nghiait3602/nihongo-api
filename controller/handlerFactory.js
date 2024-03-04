d84bbdf80dbdd4a8eca1116b414fec2a3e09c85b
Nguyen Trung Nghia with Nan <ntrungnghia102@gmail.com>
2024-03-04 21:25:19 +0700

    up

--- controller/handlerFactory.js
+++ controller/handlerFactory.js
@@ -1,6 +1,6 @@
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('./../utils/appError');
-
+/// đây chỉ là demo
 exports.deleteOne = (Model) =>
   catchAsync(async (req, res, next) => {
     const doc = await Model.findByIdAndDelete(req.params.id);
