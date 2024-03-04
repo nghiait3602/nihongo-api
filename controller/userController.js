f339a18b1c0b8abf97e5f3e676f76d48faa954f9
Nguyen Trung Nghia with Nan <ntrungnghia102@gmail.com>
2024-03-04 21:15:23 +0700

    update

--- controller/userController.js
+++ controller/userController.js
@@ -1,5 +1,5 @@
 const User = require('../model/userModel');
 const handlerFactory = require('./handlerFactory');
-
+// đây chỉ là demo
 exports.getAllUser = handlerFactory.getAll(User);
 exports.createUser = handlerFactory.createOne(User);
