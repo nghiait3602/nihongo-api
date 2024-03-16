42463a759a814dbfa794d1455afd1f220a8492d7
Pham Hong Thang <phamhongthang.1002@gmail.com>
2024-03-16 23:14:54 +0700

    xong 90% api bai hoc và update them 1 chut cho api khoa hoc

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const globalErrorHandler = require('./controller/errorController');
 
 const userRouter = require('./router/userRouter');
 const khoaHocRouter = require('./router/courseRouter');
+const baiHocRouter = require('./router/lessionRouter');
+
 const app = express();
 app.use(express.json()); // trrung gian
 app.use((req, res, next) => {
@@ -14,6 +16,7 @@ app.use((req, res, next) => {
 //router
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/khoahoc', khoaHocRouter);
+app.use('/api/v1/baihoc', baiHocRouter);
 
 app.all('*', (req, res, next) => {
   next(new AppError(`Không tìm thấy: ${req.originalUrl}`, 404));
