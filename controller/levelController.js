388a93d96d7c5393e2748b1988243fdc10e1fd60
Nguyen Trung Nghia with Nan <ntrungnghia102@gmail.com>
2024-03-22 04:19:53 +0700

    update

--- controller/levelController.js
+++ controller/levelController.js
@@ -1,11 +1,21 @@
+const catchAsync = require('../utils/catchAsync');
 const CapDo = require('./../model/levelModel');
 const factory = require('./handlerFactory');
 
-exports.setKhoaHocId = (req,res,next)=>{
-    //allow nested routes
-    if(!req.body.khoaHoc) req.body.khoaHoc = req.params.khoaHocId;
-    next();
+exports.setKhoaHocId = (req, res, next) => {
+  //allow nested routes
+  if (!req.body.khoaHoc) req.body.khoaHoc = req.params.khoaHocId;
+  next();
 };
 
-exports.getAllCapDo = factory.getAll(CapDo);
+exports.getAllCapDo = catchAsync(async (req, res, next) => {
+  const capdo = await CapDo.find();
+  res.status(200).json({
+    status: 'success',
+    data: {
+      id: 'introl',
+      capdo,
+    },
+  });
+});
 exports.creatCapDo = factory.createOne(CapDo);
--- model/levelModel.js
+++ model/levelModel.js
@@ -1,18 +1,27 @@
-const mongoose = require ('mongoose');
+const mongoose = require('mongoose');
 
 capDoSchema = new mongoose.Schema({
-    capDo:{
-      type: String,
-      require: [true, 'phải có tên cấp độ']
-    },dsKhoaHoc:[{
+  capDo: {
+    type: String,
+    require: [true, 'phải có tên cấp độ'],
+  },
+  dsKhoaHoc: [
+    {
       type: mongoose.Schema.ObjectId,
-      ref:'KhoaHoc'
-  }],
-    createAt:{
-      type:Date,
-      default: Date.now()
-    }
+      ref: 'KhoaHoc',
+    },
+  ],
+  createAt: {
+    type: Date,
+    default: Date.now(),
+  },
 });
 
+capDoSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: 'dsKhoaHoc',
+  });
+  next();
+});
 const CapDo = mongoose.model('CapDo', capDoSchema);
 module.exports = CapDo;
