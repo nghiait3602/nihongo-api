b2eb6b57464da03580dfe2277ae0f7d0a5d7a78f
Nguyen Trung Nghia with Nan <ntrungnghia102@gmail.com>
2024-03-22 21:24:13 +0700

    update

--- controller/upload.js
+++ controller/upload.js
@@ -2,6 +2,6 @@ const catchAsync = require('./../utils/catchAsync');
 
 exports.upLoad = catchAsync(async (req, res, next) => {
   const data = req.file;
-  console.log(data);
-  res.send(data);
+  console.log(data.path);
+  res.send('TEst');
 });
