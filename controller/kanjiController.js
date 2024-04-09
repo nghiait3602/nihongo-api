aa43f53827a7b155a0780321487b5efd2171698e
Pham Hong Thang <phamhongthang.1002@gmail.com>
2024-04-09 14:28:07 +0700

    tach ra thanh cong

--- controller/kanjiController.js
+++ controller/kanjiController.js
@@ -1,11 +1,12 @@
-const multer = require('multer');
-const sharp = require('sharp');
-const cloudinary = require('cloudinary').v2;
-const uploadCloud = require('../config/cloudinaryStorage.config');
-const Kanji = require('./../model/kanjiModel');
-const factory = require('./handlerFactory');
-const AppError = require('./../utils/appError');
-const catchAsync = require('./../utils/catchAsync');
+const multer = require("multer");
+const sharp = require("sharp");
+const cloudinary = require("cloudinary").v2;
+const uploadCloud = require("../config/cloudinaryStorage.config");
+const Kanji = require("./../model/kanjiModel");
+const factory = require("./handlerFactory");
+const AppError = require("./../utils/appError");
+const catchAsync = require("./../utils/catchAsync");
+const User = require('./../model/userModel');
 
 // const multerStorage = multer.memoryStorage();
 
@@ -22,11 +23,11 @@ const catchAsync = require('./../utils/catchAsync');
 //     fileFilter: multerFilter
 // });
 
-exports.uploadHinhAnhKanji = uploadCloud.single('image');
+exports.uploadHinhAnhKanji = uploadCloud.single("image");
 
 // exports.resizeHinhAnhKanji = catchAsync(async (req, res, next) => {
 //     if (!req.file) return next();
-    
+
 //     req.file.filename = `kanji-${req.params.id}-${Date.now()}.jpeg`;
 //     req.body.hinhAnhCachViet =req.file.filename;
 //     await sharp(req.file.buffer)
@@ -61,10 +62,10 @@ exports.updateImage = catchAsync(async (req, res, next) => {
   next();
 });
 
-exports.setBaiHocId = (req,res,next)=>{
-    //allow nested routes
-    if(!req.body.baiHoc) req.body.baiHoc = req.params.baiHocId;
-    next();
+exports.setBaiHocId = (req, res, next) => {
+  //allow nested routes
+  if (!req.body.baiHoc) req.body.baiHoc = req.params.baiHocId;
+  next();
 };
 
 exports.getAllKanji = factory.getAll(Kanji);
@@ -72,3 +73,37 @@ exports.getKanji = factory.getOne(Kanji);
 exports.createKanji = factory.createOne(Kanji);
 exports.updateKanji = factory.updateOne(Kanji);
 exports.deleteKanji = factory.deleteOne(Kanji);
+
+exports.addDSKanji = catchAsync(async (req, res, next) => {
+  const kanji = await Kanji.findById(req.params.id);
+  if (!kanji) return next(new AppError("No document found with that ID", 404));
+  const userInfo = await User.findById(req.user.id);
+  const dsIdKanji = userInfo.kanjiS;
+  console.log(`dsIdKanji: ${dsIdKanji}`);
+  console.log(`IdKanji: ${req.params.id}`);
+  if (dsIdKanji.some((doneKanjiId) => doneKanjiId.equals(req.params.id))) {
+    res.status(200).json({
+      status: "success",
+      data: {
+        message: "id đã có trong mảng",
+        result: 0
+      }
+    });
+  }else{
+    const updatedUser = await User.findByIdAndUpdate(
+      req.user.id,
+      { $push: { kanjiS: req.params.id } },
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+    res.status(200).json({
+      status: "success",
+      data: {
+        user: updatedUser,
+        result: 1
+      },
+    });
+  }
+});
